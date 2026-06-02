0fb25851868311eafc581d6d8eea4dcec5209f28
BishalMohapatra-bishal <bishalmohapatra123@gmail.com>
2026-06-02 21:41:05 +0530

    -nothing change

--- Reverse.java
+++ Reverse.java
@@ -1,7 +1,7 @@
 public class Reverse {
     public static void main(String[] args) {
         String reverse = "Bishal";
-        StringBuilder result = new StringBuilder();
+        StringBuilder result = new StringBuilder(); 
 
         for (int i = reverse.length() - 1; i >= 0; i--) {
             result.append(reverse.charAt(i));
