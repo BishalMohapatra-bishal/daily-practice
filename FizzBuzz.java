91ed4529e5f9fe398534f9d2a0e95ba31c1a7d3e
BishalMohapatra-bishal <bishalmohapatra168@gmail.com>
2026-06-08 23:04:53 +0530

    This program showes if a number divide by 3 it will print Fizz and if a number is divide by 5 it will print Buzz.

---
+++ FizzBuzz.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+        for (int i = 1; i <= 50; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Fizz-Buzz - " + i);
+            } else if (i % 3 == 0) {
+                System.out.println("Fuzz - " + i);
+            } else if (i % 5 == 0){
+                System.out.println("Buzz - " + i);
+            } else {
+                System.out.println("Remain Numbers - " + i);
+            }
+        }
+    }
+}
