40625cffd8cda43c0d3427dbed4a1d03ca2e5820
BishalMohapatra-bishal <bishalmohapatra168@gmail.com>
2026-06-10 22:34:44 +0530

    The program calculate the temperature.

---
+++ TemperatureConverter.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TemperatureConverter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter temperature in Celsius: ");
+        double celsius = sc.nextDouble();
+
+        // Calling our method
+        double fahrenheit = convertToFahrenheit(celsius);
+
+        System.out.println(celsius + "°C is equal to " + fahrenheit + "°F");
+    }
+
+    // This method handles the logic
+    public static double convertToFahrenheit(double c) {
+        return (c * 9.0 / 5.0) + 32;
+    }
+}
+
