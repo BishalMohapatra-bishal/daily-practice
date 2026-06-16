f5d1e357e5a69291cc0a741edafc8cb19a9b4429
BishalMohapatra-bishal <bishalmohapatra168@gmail.com>
2026-06-16 22:34:58 +0530

    Little modification

--- MovieTicket/Main.java
+++ MovieTicket/Main.java
@@ -41,9 +41,16 @@ public class Main {
         Movie avatar = new Movie("Avatar 3", 250.00, 5);
 
         TicketBookingSystem system = new TicketBookingSystem();
-        system.bookTickets(avatar, 6);
+        system.bookTickets(avatar, 4);
 
         System.out.println("Seat left for " + avatar.title + " is " + avatar.availableSeats);
+
+         Movie f1 = new Movie("F1 The movie", 350.00, 15);
+
+        // TicketBookingSystem system2 = new TicketBookingSystem();
+        system.bookTickets(f1, 6);
+
+        System.out.println("Seat left for " + f1.title + " is " + f1.availableSeats);
     }
 }
 
