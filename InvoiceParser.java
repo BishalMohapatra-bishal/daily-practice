af59bdbe546ff6d1a849922905feced3bdc0fb33
BishalMohapatra-bishal <bishalmohapatra168@gmail.com>
2026-06-19 10:59:19 +0530

    Complete the program.

--- InvoiceParser.java
+++ InvoiceParser.java
@@ -1,9 +1,33 @@
 class Main{ 
     public void processInvoice(String rawData) {
-        // String[] data = rawData.toString;
+        String[] data = rawData.split(",");
+
+        String invoiceNum = data[0];
+        String itemName = data[1];
+
+        double price = Double.parseDouble(data[2]);
+        int quantity = Integer.parseInt(data[3]);
+          
+
+
+        double totalCost = price * quantity;
+
+        System.out.println("Invoice: " + invoiceNum);
+        System.out.println("Item: " + itemName);
+        System.out.println("Price: Rs " + price);
+        System.out.println("Quantity: " + quantity);
+       
+
+
+        System.out.println("Total Cost: Rs " + totalCost);
     }
 }
 
 public class InvoiceParser {
-    
+    public static void main(String[] args) {
+        Main parser = new Main();
+        String rawLine = "INV_2026,Laptop,1200.50,2";
+
+        parser.processInvoice(rawLine);
+    }
 }
