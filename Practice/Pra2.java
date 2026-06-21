e1864d08ff02aadf16c52adf442546995e1537a2
BishalMohapatra-bishal <bishalmohapatra168@gmail.com>
2026-06-21 20:27:45 +0530

    -more practice

--- Practice/Pra2.java
+++ Practice/Pra2.java
@@ -7,9 +7,16 @@ public class Pra2 {
         // Predicate<Integer> pre2 = x -> x > 100000;
         //     System.out.println(pre2.test(1000000000));
 
-        List<Integer> l1 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8 ,9, 10, 11, 12);
-            int sum = l1.stream().filter(n -> n % 2 == 0).mapToInt(n -> n ).sum();
-            System.out.println(sum);
+        // List<Integer> l1 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8 ,9, 10, 11, 12);
+        //     int sum = l1.stream().filter(n -> n % 2 == 0).mapToInt(n -> n ).sum();
+        //     System.out.println(sum);
+
+        Predicate<String> startWithLetterB = x -> x.toLowerCase().charAt(0) == 'b';
+        Predicate<String> endsWithLetterL = x -> x.toLowerCase().charAt(x.length() - 1) == 'l';
+
+        Predicate<String> and = startWithLetterB.and(endsWithLetterL);
+        System.out.println(and.test("Bishal"));
+
 
         }
     }
