b37adc78232fe66784c6ca8f2190f06f17a97516
BishalMohapatra-bishal <bishalmohapatra168@gmail.com>
2026-07-01 23:20:22 +0530

    -add a another way to reverse a string with out inbuild methods.

--- Reverse.java
+++ Reverse.java
@@ -14,5 +14,22 @@ public class Reverse {
         System.out.println(result.toString());
 
         sc.close();
+        String getReverse = reverceInPlace("Barasa");
+        System.out.println(getReverse);
+    }
+    public static String reverceInPlace(String str) {
+        char[] chars = str.toCharArray();
+        int left = 0;
+        int right = chars.length -1;
+
+        while (left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+
+            left++;
+            right--;
+        }
+        return new String(chars);
     }
 }
