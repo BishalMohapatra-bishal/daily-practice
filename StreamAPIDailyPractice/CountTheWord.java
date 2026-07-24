78be560a7b19100eea64b4ee018aca1b00f469f2
BishalMohapatra-bishal <bishalmohapatra168@gmail.com>
2026-07-24 22:56:50 +0530

    -Solve the same problem using stream.

--- StreamAPIDailyPractice/CountTheWord.java
+++ StreamAPIDailyPractice/CountTheWord.java
@@ -1,23 +1,31 @@
 package StreamAPIDailyPractice;
 
-import java.util.HashMap;
+// import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class CountTheWord {
     public static void main(String[] args) {
 
         List<String> wordsList = List.of("Barasa", "Piggy", "Hipo", "Hathee", "Piggy", "Hipo", "Barasa");
 
-        Map<String, Integer> wordCounts = new HashMap<>();
+        // Map<String, Integer> wordCounts = new HashMap<>();
 
 
-        for (String word : wordsList) {
-            wordCounts.put(word, wordCounts.getOrDefault(word, 0) + 1);
-        }
+        // for (String word : wordsList) {
+        //     wordCounts.put(word, wordCounts.getOrDefault(word, 0) + 1);
+        // }
 
-        for(Map.Entry<String, Integer> entry : wordCounts.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
+        // for(Map.Entry<String, Integer> entry : wordCounts.entrySet()) {
+        //     System.out.println(entry.getKey() + ": " + entry.getValue());
+        // }
+        
+        Map<String, Long> wordCounts2 = wordsList.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        wordCounts2.forEach((word, count) -> 
+        System.out.println(word + ": " + count)
+    );
     }
 }
