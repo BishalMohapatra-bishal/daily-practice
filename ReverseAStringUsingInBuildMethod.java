193c796dcd9a9fd96ca063d491a3fa65986c4bc5
BishalMohapatra-bishal <bishalmohapatra168@gmail.com>
2026-07-25 10:47:06 +0530

    -Reverse the string using a inbuild method.

---
+++ ReverseAStringUsingInBuildMethod.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ReverseAStringUsingInBuildMethod {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the word: ");
+        String original = sc.nextLine();
+
+         System.out.println("Word before reverse: " + original);
+
+        StringBuilder builder = new StringBuilder(original);
+        builder.reverse();
+
+        System.out.println("Word after reverse: " + builder);
+    }
+    
+}
