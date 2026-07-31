d820e0f3ce7095bde7a07e2ecb764386275f12f5
BishalMohapatra-bishal <bishalmohapatra168@gmail.com>
2026-07-31 23:05:06 +0530

    -Complete the system design.

--- ScenarioBasedQuestions/Multithreading/MultiCounterECommerceInventoryReservationSystem.java
+++ ScenarioBasedQuestions/Multithreading/MultiCounterECommerceInventoryReservationSystem.java
@@ -2,6 +2,7 @@ package ScenarioBasedQuestions.Multithreading;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -62,7 +63,23 @@ public class MultiCounterECommerceInventoryReservationSystem {
 
         ExecutorService executor = Executors.newFixedThreadPool(5);
 
-        
+        System.out.println("---Starting Flash Sale (Initial Stock: 10) ---");
+
+        for(int i = 1; i <= 12; i++) {
+            String customerName = "Customer-" + i;
+            int requestedQty = (i % 2 == 0) ? 1 : 2;
+            executor.submit(new CustomerTask(inventory, customerName, requestedQty));
+        }
+
+        executor.shutdown();
+        try{
+            if (executor.awaitTermination(5, TimeUnit.SECONDS)) {
+                System.out.println("\n--- Sale Finished ---");
+                System.out.println("Final Remaining Stock: " + inventory.getAvailableStock());
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
 
