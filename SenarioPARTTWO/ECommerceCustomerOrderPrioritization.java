e57213569a35522c6a5218335297a9d2c761b036
BishalMohapatra-bishal <bishalmohapatra168@gmail.com>
2026-09-17 23:29:55 +0530

    Create the class only.

---
+++ SenarioPARTTWO/ECommerceCustomerOrderPrioritization.java
@@ -0,0 +1,18 @@
+package SenarioPARTTWO;
+
+class Order{
+    private int orderId;
+    private boolean isVip;
+    private double amount;
+    private int orderDaysAgo;
+
+    public Order(int orderId, boolean isVip, double amount, int orderDaysAgo) {
+        this.orderId = orderId;
+        this.isVip = isVip;
+        this.amount = amount;
+        this.orderDaysAgo =orderDaysAgo;
+    }
+}
+public class ECommerceCustomerOrderPrioritization {
+    
+}
