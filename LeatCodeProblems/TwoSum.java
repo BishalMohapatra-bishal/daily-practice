4b2f41a3e2ee2cdac5e8ae677a57494cf2e2c8ad
BishalMohapatra-bishal <bishalmohapatra168@gmail.com>
2026-09-24 22:00:05 +0530

    -little modification

--- LeatCodeProblems/TwoSum.java
+++ LeatCodeProblems/TwoSum.java
@@ -25,9 +25,10 @@ class TwoSum {
         TwoSum sol = new TwoSum();
 
         int[] nums = {2, 7, 11, 15};
-        int target = 9;
+        int target = 11;
 
         int[] result = sol.twoSum(nums, target);
         System.out.println("Indices: " + Arrays.toString(result)); 
+
     }
 }
